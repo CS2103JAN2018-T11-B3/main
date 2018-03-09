e05b6fd6dbe32b0b7ca6713c7946461ff1d66967
Shi Rong <shi_rong_1994@yahoo.com.sg>
2018-03-09 14:53:41 +0800

    Revert "Added Alias for ClearCommand"

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -61,7 +61,6 @@ public class AddressBookParser {
             return new DeleteCommandParser().parse(arguments);
 
         case ClearCommand.COMMAND_WORD:
-        case ClearCommand.COMMAND_ALIAS:
             return new ClearCommand();
 
         case FindCommand.COMMAND_WORD:
